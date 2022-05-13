10ac71daf188b2c60bdeec0771dbca9c72de082d
Hanei Islim <hanei_islim@hotmail.com>
2022-05-13 21:02:23 +0300

    Books

---
+++ Java Spring MVC/BooksAPI/src/test/java/com/HaneiIslim/MVC/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HaneiIslim.MVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
