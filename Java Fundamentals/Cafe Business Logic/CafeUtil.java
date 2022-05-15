10861ade1802e0a7d4a21d6c1bead47e10108370
Hanei Islim <hanei_islim@hotmail.com>
2022-05-15 11:22:13 +0300

    Cafe logic

---
+++ Java Fundamentals/Cafe Business Logic/CafeUtil.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+public class CafeUtil {
+    public int getStreakGoal(int numWeeks){
+        int sum = 0;
+        for(int i=0 ; i <= numWeeks; i++){
+            sum += i;
+        }
+        return sum;
+    }
+
+    public double getOrderTotal(double[] prices){
+        double sum = 0;
+        for(int i=0 ; i < prices.length; i++){
+            sum += prices[i];
+        }
+        return sum;
+
+    }
+    public void displayMenu(ArrayList<String> menuItems){
+        for(int i=0; i < menuItems.size(); i++){
+            System.out.println(i + menuItems.get(i));
+        }
+    }
+    public void addCustomer(ArrayList<String>myArray){
+        System.out.println("Please enter your name:");
+        String userName = System.console().readLine();
+        System.out.println("Hello "+ userName);
+        System.out.println("There are"+""+ myArray.size()+ ""+"people in front of you");
+        myArray.add(userName);
+    }
+
+}
