b0ed0de28c112347e2f7039201a615624c29b57c
Hanei Islim <hanei_islim@hotmail.com>
2022-05-22 13:39:45 +0300

    allbooks

---
+++ Java Spring MVC/allbooks/src/main/java/com/HaneiIslim/allBooks/AllbooksApplication.java
@@ -0,0 +1,13 @@
+package com.HaneiIslim.allBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllbooksApplication.class, args);
+	}
+
+}
